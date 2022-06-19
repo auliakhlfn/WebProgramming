759e3d1e59443c5053bc7ae944029be83cd1430a
auliakhlfn <105537070+auliakhlfn@users.noreply.github.com>
2022-06-19 21:19:36 +0700

    Add files via upload

---
+++ Pertemuan3/exercise.php
@@ -0,0 +1,19 @@
+<?php
+   $a = 10;
+   $b = 7;
+   $name = "Aulia";
+   $nim = 120103036;
+   $ab = $a * 2;    // $ab = 20
+   $bc = $b * 8;    // $bc = 56
+   $c = $ab + $bc;  // $c = 76
+
+   echo "Hasil dari $a * 2 + $b * 8 = $c";
+   echo "<br>";
+   echo "Nama : " . $name;
+   echo "</br>";
+   echo "NIM : " . $nim;
+?>
+
+
+
+   
