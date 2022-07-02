0aafd2b0b07702aa760cbabec1b3d1f4313e5b38
Aulia Khalfani <105537070+auliakhlfn@users.noreply.github.com>
2022-07-02 15:11:11 +0700

    Delete steambath directory

--- steambath/function/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server = "localhost";
-$username = "root";
-$password = "";
-$dbname = "steambath";
-
-$koneksi = mysqli_connect($server, $username, $password, $dbname) or die("Koneksi ke database gagal");
